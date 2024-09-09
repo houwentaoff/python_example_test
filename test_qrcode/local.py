e99e2ef99b5d2e9eafa79abafa28b83cda8b2fbe
Tom <544088192@qq.com>
2024-09-10 00:02:45 +0800

    增加帧种类

--- test_qrcode/local.py
+++ test_qrcode/local.py
@@ -4,6 +4,7 @@ from PIL import Image
 import struct
 import base64
 import cv2
+from PIL import Image
 begin_fmt = struct.Struct(
     'I20sI'
 )
@@ -28,6 +29,11 @@ end_frame = {
 'md5': 0,  #md5
 'total': 0, #总发送长度
 }
+file_name = ''
+md5=''
+frame_id = 0
+size = 0
+offset = 0
 # 创建mss屏幕截图对象
 with mss.mss() as sct:
     # 列出所有显示器的截图
@@ -36,19 +42,41 @@ with mss.mss() as sct:
     while True:
         screenshot = sct.grab(monitor)
         # mss返回的是一个名为sct_PIL的Image对象，可以直接显示或保存
-        from PIL import Image
+        
         img = Image.frombytes('RGB', screenshot.size, screenshot.bgra, 'raw', 'BGRX')
         # 使用pyzbar解码图像中的二维码
         decoded_objects = decode(img)
-        # 遍历解码对象
-        for obj in decoded_objects:
-            # 打印解码内容
-            #print("Type:", obj.type)
-            #print(obj.data.hex(),type(b'asa'))
-            binary_data = base64.b64decode(obj.data)
-            offset, le = struct.unpack('<II', binary_data[0:0+8])
-            print ('offset:', hex(offset), ' size:', le,' ', len(binary_data))
-            data =  binary_data[8:]
-            cv2.waitKey(800) 
+        if decoded_objects != None:     
+            # 遍历解码对象
+            for obj in decoded_objects:
+                # 打印解码内容
+                #print("Type:", obj.type)
+                #print(obj.data.hex(),type(b'asa'))
+                binary_data = base64.b64decode(obj.data)
+                #print(len(binary_data))
+                x, y = struct.unpack('<II', binary_data[0:0+8])
+                if x == 0x12345678: #begin frame
+                    frame_id = y
+                    file_name = binary_data[8:28].decode()
+                    size = struct.unpack('<I', binary_data[28:32])
+                    print('begin frame file:', file_name, ' size:', size)
+                elif x == 0x87654321: #end frame
+                    frame_id = y
+                    md5 = binary_data[8:24].decode()
+                    total = struct.unpack('<I', binary_data[24:28])[0]
+                    print('end frame, id:', frame_id, ' md5:', md5, ' already send:', total)
+                else :#data frame
+                    frame_id = x
+                    offset = y
+                    le = struct.unpack('<I', binary_data[8:12])[0]
+                    data = binary_data[12:]
+                    if le != len(data):
+                        print('frame err len:', le, ' act:', len(data))
+                    #write 2 file
+                    print('data frame id:', frame_id, ' offset:', offset, 'len:', le)
+                    
+                print ('offset:', hex(offset), ' size:', le,' ', len(binary_data))
+                data =  binary_data[8:]
+                cv2.waitKey(300) 
             #print ('offset:', offset, ' le:', le, 'data:', data.hex())
 
--- test_qrcode/remote.py
+++ test_qrcode/remote.py
@@ -5,6 +5,7 @@ import numpy as np
 import base64
 import time
 import cv2
+import hashlib
 # 定义 uImage 头部结构
 begin_fmt = struct.Struct(
     'I20sI'
@@ -17,38 +18,39 @@ end_fmt = struct.Struct(
 )
 begin_frame = {
 'magic': 0x12345678,
+'id':0,#frame num
 'file':'aaa.tar.gz', #20B
 'size': 0, #文件总大小
 } 
 data_frame = {
+'id':0,#frame num
 'offset': 0,# 文件中的偏移
 'len': 0, #该包数据帧的长度
 'data': 0,  #二进制数据
 }
 end_frame = {
 'magic': 0, # 0x87654321
-'md5': 0,  #md5
+'id':0,#frame num
+'md5': 0,  #md5 16B
 'total': 0, #总发送长度
 }
-buf=b''
-offset=0
-infd = os.open(sys.argv[1], os.O_RDONLY| os.O_BINARY);
-file_size = os.path.getsize(sys.argv[1])
-print('file size:', file_size)
-over = False
-first = True
-offset = 0
-block=1024    
-while True:
-    buf = os.read(infd, block)
-    print(len(buf))
-    size = len(buf)
-    if size != block:
-        over=True
-    data= struct.pack("<II", offset, size)
-    data += buf
+def calculate_file_md5(file_path):
+    md5 = hashlib.md5()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            md5.update(chunk)
+    return md5.hexdigest()
+def display_frame(img):
+    img = img.convert('RGB')
+    img_np = np.array(img)
+    cv2.imshow('bigbang', img_np)
+    cv2.waitKey(1000)
+    
+def gen_beginfame(frame_id, filename, size):
+    data = struct.pack("<II", 0x12345678, frame_id)
+    data += filename.encode()[0:20]
+    data += struct.pack("<I", size)
     base64_encoded = base64.b64encode(data)
-    #base64_message = #base64_encoded.decode("utf-8")
     qr = qrcode.QRCode(
         version=15,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -58,33 +60,66 @@ while True:
     qr.add_data(base64_encoded)
     qr.make(fit=True)
     img = qr.make_image(fill='black', back_color='white')
-    print('img show offset:', offset, ',len:', size, over)
-    if first:
-        first = False
+    print(frame_id, ' file:', filename, ',size:', size)
+    return img
+def gen_endfame(frame_id, md5, total):
+    data = struct.pack("<II", 0x87654321, frame_id)
+    data += md5.encode()[0:16]
+    base64_encoded = base64.b64encode(data)
+    qr = qrcode.QRCode(
+        version=15,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(base64_encoded)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    print(frame_id, 'md5:', md5, ',total:', total)
+    return img
+def gen_datafame(frame_id, offset, data, size):
+    msg = struct.pack("<III", frame_id, offset, size)
+    msg += data
+    base64_encoded = base64.b64encode(msg)
+    qr = qrcode.QRCode(
+        version=15,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(base64_encoded)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    print(frame_id, ' img show offset:', offset, ',len:', size)
+    return img
+buf=b''
+offset=0
+infd = os.open(sys.argv[1], os.O_RDONLY| os.O_BINARY);
+file_size = os.path.getsize(sys.argv[1])
+md5 = calculate_file_md5(sys.argv[1])
+print('file size:', file_size, ' md5:', md5)
 
-    img = img.convert('RGB')
-    img_np = np.array(img)
-    '''
-    # 获取原始图像的宽度和高度
-    old_width, old_height = img.size
-    # 设置缩放比例（例如，缩小到原来的一半）
-    scale_percent = 100  # 缩小到50%
-
-    # 计算新的宽度和高度
-    new_width = int(old_width * scale_percent / 100)
-    new_height = int(old_height * scale_percent / 100)
-    print('w:',old_width, 'h:', old_height, ' nw:', new_width, ' nh:', new_height)
-
-    # 设置缩放尺寸
-    dim = (new_width, new_height)
-    img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
-    resized_img = cv2.resize(img_np, dim, interpolation=cv2.INTER_AREA)
-    '''
-    cv2.imshow('qrcode', img_np)
-    cv2.waitKey(1000) 
+frame_id = 0
+img = gen_beginfame(frame_id, sys.argv[1], file_size)
+display_frame(img)
+over = False
+offset = 0
+block=1024   
+ 
+while True:
+    buf = os.read(infd, block)
+    #print(len(buf))
+    size = len(buf)
+    if size != block:
+        over=True
+    frame_id += 1
+    img = gen_datafame(frame_id, offset, buf, size)
+    display_frame(img)
 
     offset = offset + size
     if over:
         break;
-        
+frame_id += 1        
+img = gen_endfame(frame_id, md5, offset)
+display_frame(img)        
 os.close(infd)
